e543eb7c1bbba1507008b1f683280a7e39a66e24
tcfabritius <tim.fabritius@skiff.com>
2023-03-13 13:40:45 +0200

    First commit

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,17 @@
+function ListGroup() {
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Helsinki"];
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
